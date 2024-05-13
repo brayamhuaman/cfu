b1334ab71956bee898a8909de69d5947373dd9a8
Brayam <bhuamanpc@gmail.com>
2024-05-13 13:56:10 -0500

    container4

--- lib/widgets/container.dart
+++ lib/widgets/container.dart
@@ -6,30 +6,42 @@ import 'package:flutter/widgets.dart';
 class MyContainer extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    return DefaultTextStyle(
-      style: TextStyle(),
-      child: Center(
-        child:  Container(
-          width: 250,
-          height: 250,
-         
-          alignment: Alignment.bottomCenter,
-          decoration: BoxDecoration(
-          color: Colors.orange,
-          shape: BoxShape.circle,
-          ),
-         
-          padding: EdgeInsets.symmetric(
-            horizontal: 20,
-            vertical: 20,
+    return Container(
+      color: Colors.blue,
+      child: DefaultTextStyle(
+        style: TextStyle(),
+        child: Center(
+          child:  Container(
+            width: 250,
+            height: 250,
+            alignment: Alignment.bottomCenter,
+            decoration: BoxDecoration(
+            color: Colors.orange,
+            shape: BoxShape.circle,
+            boxShadow: [
+              
+              BoxShadow(
+                color: Colors.red.withOpacity(
+                  0.9,
+                ),
+                blurRadius: 10,
+                offset: Offset(0,-3),
+              ),
+            ],
             ),
-          child: Text(
-            "Brayam",
-          style: TextStyle(
-            color: Colors.cyan,
-          )
-          ),
-        ) ,
+           
+            padding: EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 20,
+              ),
+            child: Text(
+              "Brayam",
+            style: TextStyle(
+              color: Colors.cyan,
+            )
+            ),
+          ) ,
+        ),
       ),
     );
   }
